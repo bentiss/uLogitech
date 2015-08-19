0903996df8b1326dd2ad01dfca866d127b02a05d
Peter Hutterer <peter.hutterer@who-t.net>
2015-08-20 09:04:41 +1000

    Trailing whitespace fixes
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- lib/hidpp10.c
+++ lib/hidpp10.c
@@ -8,15 +8,15 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 /*
@@ -153,7 +153,7 @@ int hidpp10_request_command(int fd, union hidpp10_message *msg) {
 			break;
 		}
 	} while (ret > 0);
-	
+
 	if (ret < 0) {
 		printf("    USB error: %d\n", errno);
 		perror("write");
